96b9b057d08f2f1772fe6390f9b306187ea5b592
ortegafull87 <ortegafull87@gmail.com>
2016-05-06 00:19:11 -0500

    Se agrega GastosBean y IncidenciaBean

---
+++ app/src/main/java/com/example/victordavid/aspemovil/GastosBean.java
@@ -0,0 +1,25 @@
+package com.example.victordavid.aspemovil;
+
+/**
+ * Created by VictorDavid on 06/05/2016.
+ */
+public class GastosBean {
+    private String descripcion;
+    private Double monto;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+}
---
+++ app/src/main/java/com/example/victordavid/aspemovil/IncidenciaBean.java
@@ -0,0 +1,91 @@
+package com.example.victordavid.aspemovil;
+
+import java.util.ArrayList;
+
+/**
+ * Created by VictorDavid on 06/05/2016.
+ */
+public class IncidenciaBean {
+
+    private String claveIncidencia;
+    private String fechaIncidencia;
+    private String guardiaFalto;
+    private String guardiaSuple;
+    private String fechaHoraInicia;
+    private String fechaHoraTErmina;
+    private ArrayList<GastosBean> gastos;
+    private String uriFirma;
+    private String observaciones;
+
+    public String getClaveIncidencia() {
+        return claveIncidencia;
+    }
+
+    public void setClaveIncidencia(String claveIncidencia) {
+        this.claveIncidencia = claveIncidencia;
+    }
+
+    public String getFechaIncidencia() {
+        return fechaIncidencia;
+    }
+
+    public void setFechaIncidencia(String fechaIncidencia) {
+        this.fechaIncidencia = fechaIncidencia;
+    }
+
+    public String getGuardiaFalto() {
+        return guardiaFalto;
+    }
+
+    public void setGuardiaFalto(String guardiaFalto) {
+        this.guardiaFalto = guardiaFalto;
+    }
+
+    public String getGuardiaSuple() {
+        return guardiaSuple;
+    }
+
+    public void setGuardiaSuple(String guardiaSuple) {
+        this.guardiaSuple = guardiaSuple;
+    }
+
+    public String getFechaHoraInicia() {
+        return fechaHoraInicia;
+    }
+
+    public void setFechaHoraInicia(String fechaHoraInicia) {
+        this.fechaHoraInicia = fechaHoraInicia;
+    }
+
+    public String getFechaHoraTErmina() {
+        return fechaHoraTErmina;
+    }
+
+    public void setFechaHoraTErmina(String fechaHoraTErmina) {
+        this.fechaHoraTErmina = fechaHoraTErmina;
+    }
+
+    public ArrayList<GastosBean> getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(ArrayList<GastosBean> gastos) {
+        this.gastos = gastos;
+    }
+
+    public String getUriFirma() {
+        return uriFirma;
+    }
+
+    public void setUriFirma(String uriFirma) {
+        this.uriFirma = uriFirma;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
